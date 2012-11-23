2f976a0bfc9bc183cef6612006a49bd4952d6d9e
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-11-23 18:57:01 +0000

    - Prepped for test build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'upstream_proxy';
-$app['version'] = '1.4.4';
+$app['version'] = '1.4.5';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-upstream-proxy.spec
+++ packaging/app-upstream-proxy.spec
@@ -1,7 +1,7 @@
 
 Name: app-upstream-proxy
 Epoch: 1
-Version: 1.4.4
+Version: 1.4.5
 Release: 1%{dist}
 Summary: Upstream Proxy
 License: GPLv3
