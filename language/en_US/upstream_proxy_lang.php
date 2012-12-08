e520ef6dcdea77b4bb6ae8003762fb8a858ebb7f
Ben Chambers <bchambers@clearfoundation.com>
2012-12-08 19:17:20 +0000

    - Fixed typo

--- language/en_US/upstream_proxy_lang.php
+++ language/en_US/upstream_proxy_lang.php
@@ -2,7 +2,7 @@
 
 $lang['upstream_proxy_app_name'] = 'Upstream Proxy';
 $lang['upstream_proxy_app_description'] = 'The Upstream Proxy app provides a way to connect to upstream proxy servers.';
-$lang['upstream_proxy_wizard_description'] = 'If you need to specify upstream proxy settings to connect to the Internet, you are in the right place.  For most you, this configuration step is not necessary.';
+$lang['upstream_proxy_wizard_description'] = 'If you need to specify upstream proxy settings to connect to the Internet, you are in the right place.  For most users, this configuration step is not necessary.';
 $lang['upstream_proxy_proxy_settings'] = 'Proxy Settings';
 $lang['upstream_proxy_inline_help'] = 'The Upstream Proxy settings are used for web HTTP, HTTPS and FTP protocols.';
 
