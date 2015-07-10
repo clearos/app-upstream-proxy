80c9980689f29000a7f19629c0f6ef3b0ed82da5
pcbaldwin <pbaldwin@clearfoundation.com>
2015-07-10 13:13:43 -0400

    Prepping for snapshot build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'upstream_proxy';
-$app['version'] = '1.6.5';
+$app['version'] = '2.1.6';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
